178e6b01a06059cbd9ecf201110ed6b532e2fe7f
Udyan Singh <udyan.dev@gmail.com>
2024-10-31 00:52:41 -0700

    App Icon & FireBase

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterError.onError = (errorDetails) async {
+    FirebaseCrashlytics.instance.recordFlutterError(errorDetails, fatal: true);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+  unawaited(MobileAds.instance.initialize());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Torfin',
+      home: Scaffold(),
+    );
+  }
+}
