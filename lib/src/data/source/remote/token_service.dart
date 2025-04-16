udyan-dev/torifn

Download implemented

lib/src/data/source/remote/token_service.dart
@@ -1,0 +1,47 @@
+import 'dart:io';
+
+import 'package:torfin/core/utils/app_constant.dart';
+import 'package:torfin/src/data/source/remote/dio_service.dart';
+
+import '../../../../core/helper/app_exception.dart';
+
+class TokenService {
+  final DioService _dioService;
+
+  TokenService({required DioService dioService}) : _dioService = dioService;
+
+  Future<String> getApiToken() async {
+    try {
+      final apiKey = await _fetchApiKey();
+      if (apiKey.isEmpty) throw Exception('Failed to fetch API key.');
+      final token = await _fetchToken(apiKey);
+      if (token.isEmpty) throw Exception('Failed to fetch token.');
+      return token;
+    } on Exception catch (e) {
+      throw AppException(type: AppExceptionType.unknown, message: e.toString());
+    }
+  }
+
+  Future<String> _fetchApiKey() async {
+    final response = await _dioService.dio.get(
+      '',
+      options: _dioService.defaultOptions,
+    );
+    if (response.statusCode != HttpStatus.ok) {
+      throw Exception('Failed to fetch API key: HTTP ${response.statusCode}');
+    }
+    return AppConstant.regexForJs.firstMatch(response.data)?.group(0) ?? '';
+  }
+
+  Future<String> _fetchToken(String apiKey) async {
+    final jsFileLink = '${AppConstant.baseUrl}$apiKey';
+    final response = await _dioService.dio.get(
+      jsFileLink,
+      options: _dioService.defaultOptions,
+    );
+    if (response.statusCode != HttpStatus.ok) {
+      throw Exception('Failed to fetch token: HTTP ${response.statusCode}');
+    }
+    return AppConstant.regexForKey.firstMatch(response.data)?.group(1) ?? '';
+  }
+}

lib/src/domain/usecase/get_trending_use_case.dart
@@ -1,0 +1,57 @@
+import 'package:dio/dio.dart';
+import 'package:uuid/uuid.dart';
+
+import '../../../core/helper/app_exception.dart';
+import '../../../core/helper/base_usecase.dart';
+import '../../../core/helper/data_state.dart';
+import '../../data/model/request/trending/trending_req.dart';
+import '../../data/model/response/torrent/torrent_res.dart';
+import '../repository/snowfl_repository.dart';
+import '../repository/storage_repository.dart';
+
+class GetTrendingUseCase
+    extends BaseUseCase<List<TorrentRes>, GetTrendingUseCaseParams> {
+  final SnowflRepository snowflRepository;
+  final StorageRepository storageRepository;
+
+  GetTrendingUseCase({
+    required this.snowflRepository,
+    required this.storageRepository,
+  });
+
+  final _uuid = const Uuid();
+
+  @override
+  Future<DataState<List<TorrentRes>>> call(
+    GetTrendingUseCaseParams params, {
+    required CancelToken cancelToken,
+  }) async {
+    try {
+      final data = await Future.wait([
+        storageRepository.getToken(),
+        storageRepository.getNsfw(),
+      ]);
+      final uuid = _uuid.v4().substring(0, 8);
+      final trendingReq = TrendingReq(
+        token: data.first,
+        uuid: uuid,
+        sort: params.sort,
+        top: params.top,
+        nsfw: data.last,
+      );
+      return snowflRepository.getTrending(
+        trendingReq: trendingReq,
+        cancelToken: cancelToken,
+      );
+    } on AppException catch (e) {
+      return DataFailed(AppException(type: e.type, message: e.message));
+    }
+  }
+}
+
+class GetTrendingUseCaseParams {
+  final String top;
+  final String sort;
+
+  GetTrendingUseCaseParams({required this.top, required this.sort});
+}

udyan-dev/torfin

lib/src/data/source/remote/token_service.dart
@@ -1,0 +1,48 @@
+import 'dart:io';
+
+import 'package:torfin/src/data/source/remote/dio_service.dart';
+import 'package:torfin/src/env.dart';
+
+import '../../../../core/helper/app_exception.dart';
+import '../../../../core/utils/app_constant.dart';
+
+class TokenService {
+  final DioService _dioService;
+
+  TokenService({required DioService dioService}) : _dioService = dioService;
+
+  Future<String> getApiToken() async {
+    try {
+      final apiKey = await _fetchApiKey();
+      if (apiKey.isEmpty) throw Exception('Failed to fetch API key.');
+      final token = await _fetchToken(apiKey);
+      if (token.isEmpty) throw Exception('Failed to fetch token.');
+      return token;
+    } on Exception catch (e) {
+      throw AppException(type: AppExceptionType.unknown, message: e.toString());
+    }
+  }
+
+  Future<String> _fetchApiKey() async {
+    final response = await _dioService.dio.get(
+      '',
+      options: _dioService.defaultOptions,
+    );
+    if (response.statusCode != HttpStatus.ok) {
+      throw Exception('Failed to fetch API key: HTTP ${response.statusCode}');
+    }
+    return AppConstant.regexForJs.firstMatch(response.data)?.group(0) ?? '';
+  }
+
+  Future<String> _fetchToken(String apiKey) async {
+    final jsFileLink = '${Env.baseUrl}$apiKey';
+    final response = await _dioService.dio.get(
+      jsFileLink,
+      options: _dioService.defaultOptions,
+    );
+    if (response.statusCode != HttpStatus.ok) {
+      throw Exception('Failed to fetch token: HTTP ${response.statusCode}');
+    }
+    return AppConstant.regexForKey.firstMatch(response.data)?.group(1) ?? '';
+  }
+}

lib/src/domain/usecase/get_trending_use_case.dart
@@ -1,0 +1,57 @@
+import 'package:dio/dio.dart';
+import 'package:uuid/uuid.dart';
+
+import '../../../core/helper/app_exception.dart';
+import '../../../core/helper/base_usecase.dart';
+import '../../../core/helper/data_state.dart';
+import '../../data/model/request/trending/trending_req.dart';
+import '../../data/model/response/torrent/torrent_res.dart';
+import '../repository/torfin_repository.dart';
+import '../repository/storage_repository.dart';
+
+class GetTrendingUseCase
+    extends BaseUseCase<List<TorrentRes>, GetTrendingUseCaseParams> {
+  final TorfinRepository torfinRepository;
+  final StorageRepository storageRepository;
+
+  GetTrendingUseCase({
+    required this.torfinRepository,
+    required this.storageRepository,
+  });
+
+  final _uuid = const Uuid();
+
+  @override
+  Future<DataState<List<TorrentRes>>> call(
+    GetTrendingUseCaseParams params, {
+    required CancelToken cancelToken,
+  }) async {
+    try {
+      final data = await Future.wait([
+        storageRepository.getToken(),
+        storageRepository.getNsfw(),
+      ]);
+      final uuid = _uuid.v4().substring(0, 8);
+      final trendingReq = TrendingReq(
+        token: data.first,
+        uuid: uuid,
+        sort: params.sort,
+        top: params.top,
+        nsfw: data.last,
+      );
+      return torfinRepository.getTrending(
+        trendingReq: trendingReq,
+        cancelToken: cancelToken,
+      );
+    } on AppException catch (e) {
+      return DataFailed(AppException(type: e.type, message: e.message));
+    }
+  }
+}
+
+class GetTrendingUseCaseParams {
+  final String top;
+  final String sort;
+
+  GetTrendingUseCaseParams({required this.top, required this.sort});
+}